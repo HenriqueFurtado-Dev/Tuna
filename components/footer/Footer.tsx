164eb8a2f183c7cbae4b9ed38a6c5de6b9fc550a
Henrique Furtado <carloshenrique.furtado@hotmail.com>
2024-05-28 21:32:39 -0300

    :sparkles: feat: Add footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,11 +1,40 @@
 import React from 'react';
 import Link from 'next/link';
 import styles from '@/components/footer/Footer.module.css'; 
+import avatar from '@/public/images/avatar.png'; 
+import Image from "next/image";
 
 const Footer: React.FC = () => {
     return(
         <footer className={styles.footer}>
-            <p>Todos os direitos reservados</p>
+            <div className={styles.container}>
+                <div className={styles.members}>
+                    
+                    <div className={styles.membersIndividual}>
+                        <Image src={avatar} alt="Bruno" width={100} height={100} />
+                        <div className={styles.informations}>
+                            <h3>Bruno</h3>
+                            <p>Desenvolvedor</p>
+                        </div>
+                    </div>
+
+                    <div className={styles.membersIndividual}>
+                        <Image src={avatar} alt="Bruno" width={100} height={100} />
+                        <div className={styles.informations}>
+                            <h3>Bruno</h3>
+                            <p>Desenvolvedor</p>
+                        </div>
+                    </div>
+
+                    <div className={styles.membersIndividual}>
+                        <Image src={avatar} alt="Bruno" width={100} height={100} />
+                        <div className={styles.informations}>
+                            <h3>Bruno</h3>
+                            <p>Desenvolvedor</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </footer>
     )
 }
