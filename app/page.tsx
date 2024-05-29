cd2b695882b5effc1fcfa1a02b2a903d57ec8aca
Henrique Furtado <carloshenrique.furtado@hotmail.com>
2024-05-28 21:58:15 -0300

    :sparkles: feat: add bubbles animation in banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from '@/styles/Home.module.css'; 
 import turtles from '@/public/images/turtles.png';
+import BubbleAnimation from '@/components/home/bubbles/BubbleAnimation';
 
 export default function Home() {
   return (
     <main>
       <section className={styles.banner}>
+        <BubbleAnimation />
         <div className={styles.container}>
           <div className={styles.bannerContent}>
             <h1>Seja bem vindo ao<br/> projeto Haora</h1>
---
+++ components/home/bubbles/BubbleAnimation.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './BubbleAnimation.module.css';
+
+const BubbleAnimation: React.FC = () => {
+  return (
+    <div className={styles.ocean}>
+      {Array.from({ length: 12 }, (_, i) => (
+        <div key={i} className={`${styles.bubble} ${styles[`bubble--${i + 1}`]}`}></div>
+      ))}
+      <div id="octocat" className={styles.octocat}></div>
+    </div>
+  );
+};
+
+export default BubbleAnimation;
