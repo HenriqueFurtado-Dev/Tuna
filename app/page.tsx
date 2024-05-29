351ffc06adf111fb07efe348c8f35ca7a8ceceb0
Henrique Furtado <carloshenrique.furtado@hotmail.com>
2024-05-28 22:22:00 -0300

    :sparkles: feat: add wave animation in banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from '@/styles/Home.module.css'; 
 import turtles from '@/public/images/turtles.png';
+import waves from '@/public/images/wave.svg';
 import BubbleAnimation from '@/components/home/bubbles/BubbleAnimation';
 
 export default function Home() {
   return (
     <main>
+
       <section className={styles.banner}>
         <BubbleAnimation />
         <div className={styles.container}>
@@ -16,8 +18,20 @@ export default function Home() {
           <div className={styles.bannerImages}>
             <Image src={turtles} alt="Uma tartaruga" />
           </div>
+          
+        </div>
+        <Image className={styles.waves} src={waves} alt="Uma tartaruga" />
+      </section>
+
+      <section className={styles.about}>
+        <div className={styles.container}>
+          <div className={styles.aboutContent}>
+            <h2>Sobre o projeto</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, dolore quae facilis temporibus expedita vel consequatur soluta tempora sed veniam suscipit ea quos asperiores atque quia fugiat maiores, error eaque!</p>
+          </div>
         </div>
       </section>
+
     </main>
   );
 }
