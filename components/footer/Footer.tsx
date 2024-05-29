8c77270678632a0323f8b679c5af2b93a158a4c6
Henrique Furtado <carloshenrique.furtado@hotmail.com>
2024-05-28 21:42:54 -0300

    :lipstick: feat: Stylized footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -8,8 +8,20 @@ const Footer: React.FC = () => {
     return(
         <footer className={styles.footer}>
             <div className={styles.container}>
+
+                <div className={styles.navigation}>
+                    <h3>Navegação pelo site</h3>
+                    <nav>
+                        <ul>
+                            <li>Inicio</li>
+                            <li>Sobre</li>
+                            <li>Solucao</li>
+                            <li>Contato</li>
+                        </ul>
+                    </nav>
+                </div>
+
                 <div className={styles.members}>
-                    
                     <div className={styles.membersIndividual}>
                         <Image src={avatar} alt="Bruno" width={100} height={100} />
                         <div className={styles.informations}>
@@ -33,6 +45,9 @@ const Footer: React.FC = () => {
                             <p>Desenvolvedor</p>
                         </div>
                     </div>
+                </div>
+                <div className={styles.credits}>
+                    <p>Todos os direitos reservados - Global Solution | FIAP 2024</p>
                 </div>
             </div>
         </footer>
