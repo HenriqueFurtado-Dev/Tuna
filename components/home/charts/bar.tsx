a16735254b0da95d07283579140c645f4a36ee38
Henrique F <carloshenrique.furtado@hotmail.com>
2024-06-07 16:28:40 -0300

    Refactor charts

--- components/home/charts/bar.tsx
+++ components/home/charts/bar.tsx
@@ -1,66 +1,29 @@
-'use client'
+// nextjs13 - works
+"use client";
+import { useEffect, useState } from "react";
 
-// Importe dynamic do Next.js
-import dynamic from 'next/dynamic';
+export default function ApexChart(props: any) {
+  const [Chart, setChart] = useState<any>();
+  const hasType = typeof props?.type !== "undefined";
 
-// Importe ApexCharts como um componente dinâmico com { ssr: false }
-const ReactApexChart = dynamic(() => import('react-apexcharts'), { ssr: false });
-
-// Resto do seu código aqui
-import React from 'react';
-import styles from './Chart.module.css';
-import { ApexOptions } from 'apexcharts';
-
-// Define os tipos para as props e o state
-interface ApexChartProps {}
-
-interface ApexChartState {
-  series: Array<{
-    name: string;
-    data: number[];
-  }>;
-  options: ApexOptions;
-}
-
-class ApexChart extends React.Component<ApexChartProps, ApexChartState> {
-  constructor(props: ApexChartProps) {
-    super(props);
-
-    this.state = {
-      series: [{
-        name: 'Lixo nos Oceanos',
-        data: [100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650]
-      }],
-      options: {
-        chart: {
-          height: 350,
-          type: 'line' as 'line', // Definindo explicitamente como 'line'
-        },
-        dataLabels: {
-          enabled: false,
-        },
-        stroke: {
-          curve: 'smooth'
-        },
-        xaxis: {
-          categories: ["1920", "1930", "1940", "1950", "1960", "1970", "1980", "1990", "2000", "2010", "2020", "2030"],
-        },
-        legend: {
-          labels: {
-            colors: '#ffffff' // Alteração da cor das legendas para branco
-          }
-        }
-      }
+  useEffect(() => {
+    const fetchChart = async () => {
+      const mod = await import("react-apexcharts");
+      setChart(mod.default);
     };
-  }
 
-  render() {
-    return (
-      <div className={styles.bar}>
-        <ReactApexChart options={this.state.options} series={this.state.series} type="line" height={350} />
-      </div>
-    );
-  }
+    fetchChart();
+  }, []);
+
+  return hasType && Chart && (
+    <div className={props.className}>
+      <Chart
+        options={props.options}
+        series={props.series}
+        type={props.type}
+        width={props.width}
+        height={props.height}
+      />
+    </div>
+  );
 }
-
-export default ApexChart;
--- components/home/charts/circle.tsx
+++ components/home/charts/circle.tsx
@@ -1,81 +1,52 @@
-'use client' 
+// nextjs13 - works
+"use client";
+import { useEffect, useState } from "react";
 
-import dynamic from 'next/dynamic';
+export default function ApexChart(props: any) {
+  const [Chart, setChart] = useState<any>();
+  const hasType = typeof props?.type !== "undefined";
 
-// Importe ApexCharts como um componente dinâmico com { ssr: false }
-const ApexCharts = dynamic(() => import('react-apexcharts'), { ssr: false });
-
-
-import React from 'react';
-import ReactApexChart from 'react-apexcharts';
-import styles from './Chart.module.css';
-import { ApexOptions } from 'apexcharts';
-
-// Define os tipos para as props e o state
-interface ApexChartProps {}
-
-interface ApexChartState {
-  series: number[]; // Série de dados é um array de números
-  options: ApexOptions;
-}
-
-class ApexChart extends React.Component<ApexChartProps, ApexChartState> {
-  constructor(props: ApexChartProps) {
-    super(props);
-
-    this.state = {
-      series: [14.1, 11.9, 9.4, 9.1, 7.9, 7.6, 6.1, 33.9], // Porcentagens dos principais tipos de lixo
-      options: {
-        chart: {
-          width: 380,
-          type: 'pie',
-        },
-        labels: [
-          'Sacolas plásticas descartáveis', 
-          'Garrafas plásticas', 
-          'Embalagens de comida e talheres', 
-          'Pacotes', 
-          'Corda sintética', 
-          'Equipamento de pesca', 
-          'Tampas ou lacres de plástico', 
-          'Outros'
-        ],
-        fill: {
-          opacity: 1
-        },
-        stroke: {
-          width: 1,
-          colors: undefined
-        },
-        legend: {
-          position: 'bottom',
-          labels: {
-            colors: '#ffffff' // Alteração da cor das legendas para branco
-          }
-        },
-        theme: {
-          monochrome: {
-            enabled: true,
-            shadeTo: 'light',
-            shadeIntensity: 0.6
-          }
-        }
-      }
+  useEffect(() => {
+    const fetchChart = async () => {
+      const mod = await import("react-apexcharts");
+      setChart(mod.default);
     };
-  }
 
-  render() {
-    return (
-      <div className={styles.circle}>
-        <ReactApexChart 
-          options={this.state.options} 
-          series={this.state.series} 
-          type="pie" 
-          width={380} 
-        />
-      </div>
-    );
-  }
+    fetchChart();
+  }, []);
+
+  return hasType && Chart && (
+    <div>
+      <Chart
+        type="pie"
+        width={380}
+        series={[14.1, 11.9, 9.4, 9.1, 7.9, 7.6, 6.1, 33.9]}
+        options={{
+          labels: [
+            'Sacolas plásticas descartáveis',
+            'Garrafas plásticas',
+            'Embalagens de comida e talheres',
+            'Pacotes',
+            'Corda sintética',
+            'Equipamento de pesca',
+            'Tampas ou lacres de plástico',
+            'Outros'
+          ],
+          legend: {
+            position: 'bottom',
+            labels: {
+              colors: '#ffffff' // Alteração da cor das legendas para branco
+            }
+          },
+          theme: {
+            monochrome: {
+              enabled: true,
+              shadeTo: 'light',
+              shadeIntensity: 0.6
+            }
+          }
+        }}
+      />
+    </div>
+  );
 }
-
-export default ApexChart;
